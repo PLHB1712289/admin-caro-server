68b9b8d4d1302006c56b7c99593decf0739db824
PLHB1712289 <57471680+PLHB1712289@users.noreply.github.com>
2020-12-04 15:31:52 +0700

    init app

---
+++ config.js
@@ -0,0 +1,25 @@
+const PRODUCTION_ENVIRONMENT = {
+  title: "production environment",
+  URL_CLIENT: process.env.URL_CLIENT,
+};
+
+const DEVELOPMENT_ENVIRONMENT = {
+  title: "development environment",
+  URL_CLIENT: "http://localhost:4001",
+};
+
+const ENVIRONMENT =
+  process.env.ENVIRONMENT === "PRODUCTION"
+    ? { ...PRODUCTION_ENVIRONMENT }
+    : { ...DEVELOPMENT_ENVIRONMENT };
+
+//
+console.log(
+  `Run on ${
+    process.env.ENVIRONMENT === "PRODUCTION"
+      ? "PRODUCTION ENVIRONMENT"
+      : "DEVELOPMENT ENVIRONMENT"
+  }`
+);
+
+module.exports = { ENVIRONMENT };
