7ca06942531e934f63dba6e3dc9491ae0cc011af
PLHB1712289 <57471680+PLHB1712289@users.noreply.github.com>
2020-12-09 18:17:00 +0700

    try to find bug in user-router

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const passport = require("passport");
 const session = require("express-session");
 const bodyParser = require("body-parser");
 
-const morgan=require('morgan');
+const morgan = require("morgan");
 
 const createError = require("http-errors");
 const path = require("path");
@@ -17,10 +17,9 @@ const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 
 const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/userRoute");
+// const usersRouter = require("./routes/userRoute");
 
 const app = express();
-
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -30,15 +29,15 @@ app.set("view engine", "hbs");
 require("./database").connect();
 //middleware
 app.use(cors());
-app.use(morgan('tiny'));
+app.use(morgan("tiny"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
-    session({
-        secret: "secretcode",
-        resave: true,
-        saveUninitialized: true,
-    })
+  session({
+    secret: "secretcode",
+    resave: true,
+    saveUninitialized: true,
+  })
 );
 app.use(passport.initialize());
 app.use(passport.session());
@@ -51,14 +50,12 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/user", usersRouter);
+// app.use("/user", usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
 });
-
-
 
 // error handler
 app.use(function (err, req, res, next) {
